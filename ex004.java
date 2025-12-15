802b7860505d43f11d7de509ccbe38ae90a2233a
paivagpg77 <paivag171@gmail.com>
2025-12-15 19:57:12 -0300

    ㅤ

---
+++ ex004.java
@@ -0,0 +1,15 @@
+public class Ex004{
+    public static void main(String[] args){
+        //Tipo da variavel , Nome da Varialvel , Contéudo da variavel
+        String variavel = "Olá";
+        int number1 = 10;
+        double percent = 1.5;
+        boolean isDriver = true;
+
+
+        System.out.println(variavel);
+        System.out.println(number1);
+        System.out.println(percent);
+        System.out.println(isDriver);
+    }
+}
