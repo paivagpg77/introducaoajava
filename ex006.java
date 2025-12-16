e057c335cd524c29dcb782b94bab6270fa66931a
paivagpg77 <paivag171@gmail.com>
2025-12-16 17:16:20 -0300

    ㅤ

---
+++ ex006.java
@@ -0,0 +1,21 @@
+package estudosjava;
+import java.util.Scanner;
+
+public class Ex006{
+    public static void main(String[] args){
+       Scanner sc =  new Scanner(System.in);
+
+       System.out.println("Digite seu primeiro Nome: ");
+       String x = sc.nextLine();
+
+       System.out.println("Digite seu segundo Nome: ");
+       String y = sc.nextLine();
+
+       name(x,y);
+       sc.close();
+    }
+
+    static void name(String x, String y) {
+    	System.out.println("Seu primeiro nome é "+  x  +" seu segundo nome é "+  y);
+    }
+}
