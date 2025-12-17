a15be28cb129b2af4c57f0e17b01e7e6cc5d764c
paivagpg77 <paivag171@gmail.com>
2025-12-17 20:35:40 -0300

    Utilizando For

--- geradordamegasena2.java
+++ geradordamegasena2.java
@@ -4,7 +4,7 @@ public class GeradorDaMegaSena2 {
 	public static void main(String[] args) {
 		Random generate = new Random();
 		
-		
+		//Utilização do For para geração
 		for(int i = 0; i < 6; i++) {
 			int number = generate.nextInt(60);
 			System.out.println("Número "+(i+1)+" :"+number);
